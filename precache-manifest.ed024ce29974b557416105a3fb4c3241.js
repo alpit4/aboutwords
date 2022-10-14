3b6d10ee3ae8a82849e5ebe521d2e3d4839bf65e
alpit4 <109150699+alpit4@users.noreply.github.com>
2022-10-14 21:42:53 +0530

    Add files via upload

---
+++ precache-manifest.ed024ce29974b557416105a3fb4c3241.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9dd31d61164fac154d15",
+    "url": "/static/css/main.7c31ba9b.chunk.css"
+  },
+  {
+    "revision": "9dd31d61164fac154d15",
+    "url": "/static/js/main.9dd31d61.chunk.js"
+  },
+  {
+    "revision": "8f2f4211b0891106f089",
+    "url": "/static/js/1.8f2f4211.chunk.js"
+  },
+  {
+    "revision": "a2b9411c293c257fd36d",
+    "url": "/static/js/2.a2b9411c.chunk.js"
+  },
+  {
+    "revision": "769c94daf783692f2c65",
+    "url": "/static/js/runtime~main.769c94da.js"
+  },
+  {
+    "revision": "8af5e7608754b223ef635fd53a668a21",
+    "url": "/index.html"
+  }
+];
